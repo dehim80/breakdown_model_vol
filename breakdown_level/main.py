5a02755bf38c4d21c32ab000275dba5bba41d84d
dehim80 <dehim80@gmail.com>
2022-07-17 20:37:41 +0300

    add create channel

--- breakdown_level/main.py
+++ breakdown_level/main.py
@@ -1,10 +1,48 @@
+import json, time
 import requests
+from time_modul import time_mod_midnight, time_now, time_mod
+
+def main():
+    create_channel()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def create_channel():
+    # Каждые 5 минут сканирует предыдущие 4 часа на наличие узкого коридора. Начиная с 4:30.Возвращает границы
+    # С 00 до 4-30 перерыв в виду малой активности рынкаtime_mod_midnight()
+    time_mod_midnight()
+    url = (f'https://www.bitmex.com/api/v1/trade/bucketed?binSize=5m&partial=false&symbol=XBTUSD'
+           f'&columns=high%2C%20low&count=48&reverse=true&endTime={time_now()}')
+    resp = requests.get(url).text
+    data = json.loads(resp)
+    high_elts = []
+    low_elts = []
+    for h in range(48):
+        high_elts.append(data[h]['high'])
+        low_elts.append(data[h]['low'])
+    high_elts.sort(reverse=True)
+    low_elts.sort()
+    print(f'high= {high_elts[0]}   low= {low_elts[0]}')
+    diff = high_elts[0] - low_elts[0]
+    high_el = high_elts[0]
+    low_el = low_elts[0]
+    if diff > 200:
+        time_mod()
+    else:
+        return check_activ_ord(high_el, low_el)
+
+
+def check_activ_ord(high_el, low_el):
+    # Следим за ценой и открываем ордер
+    k = 1
+    while True:
+        url = 'https://www.bitmex.com/api/v1/trade?symbol=XBTUSD&count=1&reverse=true'
+
+        resp = requests.get(url).text
+        data = json.loads(resp)
+        data1 = (data[0]['price'])
+        print(k,data1)
+        time.sleep(2)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
---
+++ breakdown_level/time_modul.py
@@ -0,0 +1,67 @@
+import time
+
+
+def time_mod():
+    # Функция ждет, пока минуты будут кратные пяти. Затем выходит из функции.
+    loc_hour = time.localtime().tm_hour
+    loc_min = time.localtime().tm_min
+
+    print(f'Мы зашли в фунцию time_mod {loc_hour}:{loc_min}')
+    time.sleep(100)
+    while True:
+        local_time = time.localtime().tm_min
+        loc2 = local_time % 5
+        if (loc2 == 0) or local_time == 0:
+            time.sleep(5)
+            return
+        else:
+            time.sleep(10)
+
+
+def time_mod_midnight():
+    # Следит за временем в 04-30 делает return
+    loc_hour = time.localtime().tm_hour
+    if loc_hour >= 4 and loc_hour <= 23:
+        return
+    while True:
+        loc_hour = time.localtime().tm_hour
+        loc_min = time.localtime().tm_min
+        print(f'Мы зашли в функцию time_mod_midnight {loc_hour}:{loc_min}')
+        if loc_hour == 4 and loc_min == 30:  # Должно быть loc_hour == 4 loc_min == 0
+            return
+        else:
+            time_mod()
+        print(f'Мы вернулись в функцию time_mod_midnight {loc_hour}:{loc_min}')
+
+
+def time_now():
+    print('Зашли в функцию time_nowW')
+    loc_hour = time.gmtime().tm_hour
+    loc_min = time.gmtime().tm_min
+    loc_date = time.gmtime().tm_mday
+    loc_month = time.gmtime().tm_mon
+    loc_year = time.gmtime().tm_year
+    return (f'{loc_year}-{loc_month}-{loc_date}%20{loc_hour}%3A{loc_min}')
+
+
+def time_four_hours_ago():
+    print('time_four_hours_ago')
+    loc_hour = (time.gmtime().tm_hour)-4
+    loc_min = time.gmtime().tm_min
+    loc_date = time.gmtime().tm_mday
+    loc_month = time.gmtime().tm_mon
+    loc_year = time.gmtime().tm_year
+    print("4 hours ago ", f'{loc_year}-{loc_month}-{loc_date}%20{loc_hour}%3A{loc_min}')
+    return (f'{loc_year}-{loc_month}-{loc_date}%20{loc_hour}%3A{loc_min}')
+
+
+def date_now():
+    loc_year = time.gmtime().tm_year
+    loc_month = time.gmtime().tm_mon
+    loc_date = time.gmtime().tm_mday
+    return (f'{loc_year}-{loc_month}-{loc_date}')
+
+
+# if __name__ == '__main__':
+#     print(time_four_hours_ago())
+#     print(time_now())
