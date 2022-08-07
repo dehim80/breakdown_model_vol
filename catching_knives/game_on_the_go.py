c60abc8f7a7d7cc1e32202094bd7bca0f5cc7ea2
dehim80 <dehim80@gmail.com>
2022-08-07 20:27:10 +0300

    add try-except construction

--- catching_knives/game_on_the_go.py
+++ catching_knives/game_on_the_go.py
@@ -20,36 +20,34 @@ def check_big_candle():
         min1=time_now()[5:7]
         if min2!=min1:
             s=1
-
         url = f'https://www.bitmex.com/api/v1/trade?symbol=XBTUSD&columns=price&count=1000&start={s}&reverse=false&' \
               f'startTime={date_now()}%20{time_now()}'
+        try:
+            resp = requests.get(url).text
+            data = json.loads(resp)
+            lenght = len(data)
+            if s==1 and lenght != 0:
+                price_first = data[0]['price']
+                print('price_first=', price_first)
+            s = s+lenght
+            min2 = time_now()[5:7]
 
-        resp = requests.get(url).text
-        data = json.loads(resp)
-        lenght = len(data)
-        if s==1 and lenght != 0:
-            price_first = data[0]['price']
-            print('price_first=', price_first)
-        s = s+lenght
-        print(time_now())
-        print('len data=', lenght)
-        min2 = time_now()[5:7]
-
-        for i in range(lenght):
-            price = data[i]['price']
-            timee= data[i]['timestamp'][11:19]
-            print(f'time={timee}  price={price}')
-            if abs(price_first-price)>150:
-                if price_first - price>0:
-                    orderQty = -100
-                else:
-                    orderQty = 100
-                print('У нас нужное изменение цены, открываем маркет')
-                with open("infa_onthe_orders.txt", "a") as file:
-                    file.write(f"\ntime={timee}  price={price}  order-{orderQty}")
-                return new_market_order(orderQty)
-
-        print('s=', s)
+            for i in range(lenght):
+                price = round(data[i]['price'])
+                timee= data[i]['timestamp'][11:19]
+                print(f'time={timee}  price={price}  delta={abs(price_first-price)}')
+                if abs(price_first-price)>150:
+                    if price_first - price>0:
+                        orderQty = -100
+                    else:
+                        orderQty = 100
+                    print('У нас нужное изменение цены, открываем маркет')
+                    return new_market_order(orderQty)
+        except :
+            print('connection error')
+            time.sleep(100)
+            check_big_candle()
+        # print('s=', s)
 
 # ---------------------------------------------------------------------------------------
 # Модуль отвечает за манипуляции с ордерами
@@ -58,26 +56,26 @@ def new_market_order(orderQty):
     print(f'Мы вошли в функцию new_market_order сторона: {orderQty}')
     client = bitmex.bitmex(test=False, api_key='A8n4hPQLXfbnKy4CONZwQAPl',
                            api_secret='8aNSZRFE3ZzwbzMSyiqLXIzZTgeJUF6cUmXjrgTYW_1xjrfd')
-    order_new_market = client.Order.Order_new(symbol='XRPUSD', orderQty=orderQty).result()
+    order_new_market = client.Order.Order_new(symbol='XBTUSD', orderQty=orderQty).result()
     ord_new_m_price = order_new_market[0]['price']
     orderID = order_new_market[0]['orderID']
     side = order_new_market[0]['side']
     timestamp = order_new_market[0]['timestamp']
-    print (f'orderID: {orderID}')
+    print (f'price: {ord_new_m_price} orderID: {orderID}')
     with open("market_order.txt", "a") as file:
         file.write(f"\norderID: {orderID}, price:{ord_new_m_price}, order:{orderQty},time:{timestamp}")
     with open("trades_history.txt", "a") as file:
         file.write(f"\nnew_market_order - time:{timestamp}, price:{ord_new_m_price}, side:{side},orderID: {orderID}")
     if orderQty > 0:
-        ord_lim_price = ord_new_m_price + 0.0003 # +200 для биткоина
-        orderQty = -1 # -100для биткоина
-        stopPX = ord_new_m_price - 0.0003 # 100 для биткоина
+        ord_lim_price = ord_new_m_price + 200 # +200 для биткоина 
+        orderQty = -100 # -100для биткоина
+        stopPX = ord_new_m_price - 100 # 100 для биткоина
         new_limit_order(orderQty, ord_lim_price)
         check_open_ord(orderQty, stopPX, ord_lim_price, orderID)
     else:
-        ord_lim_price = ord_new_m_price - 0.0003
-        orderQty = +1
-        stopPX = ord_new_m_price + 0.0003 # 100 для биткоина
+        ord_lim_price = ord_new_m_price - 200
+        orderQty = +100
+        stopPX = ord_new_m_price + 100 # 100 для биткоина
         new_limit_order(orderQty, ord_lim_price)
         check_open_ord(orderQty, stopPX, ord_lim_price, orderID)
 
@@ -87,8 +85,7 @@ def new_limit_order(orderQty, price):
     print(f'Мы вошли в функцию new_limit_order сторона: {orderQty} цена: {price} ')
     client = bitmex.bitmex(test=False, api_key='A8n4hPQLXfbnKy4CONZwQAPl',
                            api_secret='8aNSZRFE3ZzwbzMSyiqLXIzZTgeJUF6cUmXjrgTYW_1xjrfd')
-    order_new = client.Order.Order_new(symbol='XRPUSD', orderQty=orderQty, ordType='Limit', price=price).result()
-    print('order_new - ',order_new)
+    order_new = client.Order.Order_new(symbol='XBTUSD', orderQty=orderQty, ordType='Limit', price=price).result()
     orderID = order_new[0]['orderID']
     side = order_new[0]['side']
     timestamp = order_new[0]['timestamp']
@@ -96,7 +93,7 @@ def new_limit_order(orderQty, price):
     with open("limit_order.txt", "a") as file:
         file.write(f"\norderID: {orderID}, price:{price}, side:{side},time:{timestamp}")
     with open("trades_history.txt", "a") as file:
-        file.write(f"\nnew_limit_order - time:{timestamp}, price:{price}, side:{side},orderID: {orderID}")
+        file.write(f"\nnew_limit_order   - time:{timestamp}, price:{price}, side:{side},orderID: {orderID}")
     print('лимитник выставлен  orderID: ',order_new[0]['orderID'])
     return
 
@@ -107,7 +104,7 @@ def check_open_ord(orderQty, stopPX, ord_lim_price, orderID):
     print(f'сторона: {orderQty} стоп-цена: {stopPX}\n'
           f'orderID {orderID} лимит-цена: {ord_lim_price}')
     while True:
-        url = 'https://www.bitmex.com/api/v1/trade?symbol=XRPUSD&count=1&reverse=true'
+        url = 'https://www.bitmex.com/api/v1/trade?symbol=XBTUSD&count=1&reverse=true'
 
         resp = requests.get(url).text
         data = json.loads(resp)
@@ -127,19 +124,19 @@ def check_open_ord(orderQty, stopPX, ord_lim_price, orderID):
                 close_position() #-100 для биткоина
                 return
 
+
 def close_position():
     print(f'Мы вошли в функцию close_position')
     client = bitmex.bitmex(test=False, api_key='A8n4hPQLXfbnKy4CONZwQAPl',
                                api_secret='8aNSZRFE3ZzwbzMSyiqLXIzZTgeJUF6cUmXjrgTYW_1xjrfd')
-    close_pos = client.Order.Order_closePosition(symbol='XRPUSD').result()
+    close_pos = client.Order.Order_closePosition(symbol='XBTUSD').result()
     print(f'type close_position = {type(close_pos)}')
     orderID = close_pos[0]['orderID']
     side = close_pos[0]['side']
     timestamp = close_pos[0]['timestamp']
     price = close_pos[0]['price']
     with open("trades_history.txt", "a") as file:
-        file.write(f"\nclose_position -  time:{timestamp}, price:{price}, side:{side},orderID: {orderID}")
-
+        file.write(f"\nclose_position   -  time:{timestamp}, price:{price}, side:{side},orderID: {orderID}")
 
 
 def close_all_position():
@@ -151,8 +148,6 @@ def close_all_position():
     close_all = client.Order.Order_cancelAll().result()
     close_all= close_all[0]
     if len(close_all)>0:
-        print(type(close_all))
-        print(close_all)
         timestamp = close_all[0]['timestamp']
         orderID = close_all[0]['orderID']
         side = close_all[0]['side']
@@ -195,6 +190,5 @@ def date_now():
 #-------------------------------------------------------------------------------------------------------
 
 if __name__=='__main__':
-    close_all_position()
+    check_big_candle()
     #new_market_order(-1)
-    #check_big_candle()
